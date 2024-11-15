9cd8e89188fa72fae06f5f2dbdab07c0cb52db68
Crhystel <Crhystel@DESKTOP-IJ6GBVS>
2024-11-15 08:43:34 -0500

    Agregar archivos de proyecto.

---
+++ CabreraVelascoProyectoLayout/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CabreraVelascoProyectoLayout
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
