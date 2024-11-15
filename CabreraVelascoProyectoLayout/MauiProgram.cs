5eb3c50d185272f0857c31e398b39c6810f2a7cd
Crhystel <crhystelvelasco213@gmail.com>
2024-11-15 17:12:05 -0500

    estilos de stack listos, añadi diseños de fuentes

--- CabreraVelascoProyectoLayout/MauiProgram.cs
+++ CabreraVelascoProyectoLayout/MauiProgram.cs
@@ -13,6 +13,9 @@ namespace CabreraVelascoProyectoLayout
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Pacifico.ttf", "Pacifico");
+                    fonts.AddFont("Amatic-Bold.ttf", "AmaticBold");
+                    fonts.AddFont("AmaticSC-Regular.ttf", "Amatic");
                 });
 
 #if DEBUG
